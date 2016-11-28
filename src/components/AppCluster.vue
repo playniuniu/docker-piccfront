9990f9d8e465f3f2f6fe34f64be0e198260e4bba
playniuniu <playniuniu@gmail.com>
2016-11-28 23:51:51 +0800

    change echarts setting

--- src/components/AppCluster.vue
+++ src/components/AppCluster.vue
@@ -1,13 +1,17 @@
 <template>
     <section class="chart-container">
-        <baidu-echarts></baidu-echarts>
+        <baidu-echarts :mapdata="mapdata"></baidu-echarts>
     </section>
     
 </template>
 
 <style>
+    .app-container {
+        background-color: #00497F;
+    }
+
     .chart-container {
-        height: 100%;
+        padding: 40px 30px 0;
     }
 </style>
 
@@ -16,5 +20,17 @@
 
     export default {
         components: { BaiduEcharts },
+        data() {
+            return {
+                mapdata: [
+                    { name: '北京', value: 1930 },
+                    { name: '上海', value: 680 },
+                    { name: '南京', value: 1465 },
+                    { name: '成都', value: 303 },
+                    { name: '广州', value: 1103 },
+                    { name: '兰州', value: 933 }
+                ],
+            }
+        }
     }
 </script>
--- src/utilities/BaiduEcharts.vue
+++ src/utilities/BaiduEcharts.vue
@@ -3,14 +3,15 @@
 </template>
 
 <script>
-    import echarts from 'echarts';
-    import axios from 'axios';
+    import echarts from 'echarts'
+    import axios from 'axios'
+    import ChinaCityGeo from './ChinaCityGeo.js'
 
     export default {
-        props: [ 'edata' ],
+        props: [ 'mapdata' ],
         data() {
             return {
-                baidu_echarts: null
+                baidu_echarts: null,
             }
         },
         mounted() {
@@ -21,11 +22,103 @@
                 axios.get('/assets/echarts/china.json').then( (response) => {
                     echarts.registerMap('china', response.data);
                     this.baidu_echarts = echarts.init(this.$el);
-                    axios.get('/assets/echarts/option.json').then( (response) => {
-                        this.baidu_echarts.setOption(response.data);
-                    });
+                    let option_data = this.convertData(this.mapdata);
+                    this.baidu_echarts.setOption(this.initOption(option_data));
                 });
-            }
+            },
+            convertData(conv_data) {
+                let res = [];
+                for (let i=0; i < conv_data.length; i++) {
+                    let geoCoord = ChinaCityGeo[conv_data[i].name];
+                    if (geoCoord) {
+                        res.push({
+                            name: conv_data[i].name,
+                            value: geoCoord.concat(conv_data[i].value)
+                        });
+                    }
+                }
+                return res;
+            },
+            initOption(option_data) {
+                let option = {
+                    backgroundColor: '#00497F',
+                    title: {
+                        text: 'PICC 虚机概况',
+                        x:'center',
+                        textStyle: {
+                            fontSize: 24,
+                            color: '#fff'
+                        }
+                    },
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: (params) => {
+                            return params.name + ' : ' + params.value[2] + ' 个虚机';
+                        }
+                    },
+                    legend: {
+                        orient: 'vertical',
+                        y: 'bottom',
+                        x:'right',
+                        data:['PICC 虚机概况'],
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    visualMap: {
+                        min: 0,
+                        max: 2000,
+                        splitNumber: 5,
+                        inRange: {
+                            color: ['#50a3ba', '#eac736', '#d94e5d']
+                        },
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    geo: {
+                        map: 'china',
+                        label: {
+                            emphasis: {
+                                show: false
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                areaColor: '#fff',
+                                borderColor: '#fff'
+                            },
+                            emphasis: {
+                                areaColor: '#fff'
+                            }
+                        }
+                    },
+                    series: [
+                        {
+                            name: 'PICC 虚机概况',
+                            type: 'scatter',
+                            coordinateSystem: 'geo',
+                            data: option_data,
+                            symbolSize: 12,
+                            label: {
+                                normal: {
+                                    show: false
+                                },
+                                emphasis: {
+                                    show: false
+                                }
+                            },
+                            itemStyle: {
+                                emphasis: {
+                                    borderColor: '#fff',
+                                    borderWidth: 1
+                                }
+                            }
+                        }
+                    ]
+                }
+                return option;
+            } 
         }
     };
 </script>
